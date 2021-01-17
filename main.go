e379993d10be17e73403d9280fb6490ecdfe7b33
Josh Lowy <26966954+DLJ42@users.noreply.github.com>
2021-01-17 00:16:32 -0500

    refinements
    spring cleaning

--- main.go
+++ main.go
@@ -14,22 +14,22 @@ https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/04.1.ht
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "<h1>Hello All</h1>")
-	t, _ := template.Parsefiles("root.gtpl")
-	t.Execute()
+	t,_ := template.ParseFiles("root.gtpl")
+	t.Execute(w, nil)
 
 }
 
 func search(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Method: " + r.Method)
-
+	t,_ := template.ParseFiles("search.gtpl")
 	if r.Method == "GET" {
-		t,_ := template.ParseFiles("search.gtpl")
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
-
 		// process data
 		fmt.Println("Search Query: ", r.Form["searchQuery"])
+		t.Execute(w, nil)
+
 	}
 }
 
@@ -37,5 +37,6 @@ func main() {
 	fmt.Println("hello all")
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/search", search)
+	fmt.Println("listening on 0.0.0.0:8088")
 	http.ListenAndServe(":8088", nil)
 }
